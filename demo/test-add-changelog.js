392b6759f70752cc86c6084f811075f43f3a31d5
hayden <i@wuhaidong.me>
2022-05-10 18:12:12 +0800

    feat: 新增changelog 生产

---
+++ demo/test-add-changelog.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion :
+ * @Author       : wuhaidong
+ * @Date         : 2022-05-10 18:10:35
+ * @LastEditors  : wuhaidong
+ * @LastEditTime : 2022-05-10 18:11:08
+ */
+console.log('change log')
