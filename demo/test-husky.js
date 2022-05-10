80bcedf4bfdbdbc725cd0ef44886a478640b6d8f
hayden <i@wuhaidong.me>
2022-05-10 16:40:39 +0800

    check style

---
+++ demo/test-husky.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion :
+ * @Author       : wuhaidong
+ * @Date         : 2022-05-10 16:38:22
+ * @LastEditors  : wuhaidong
+ * @LastEditTime : 2022-05-10 16:39:15
+ */
+const kongFunction = (a = {})
