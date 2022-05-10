d8f704be7c7bb9b83ab94762847151f6a6c80edc
hayden <i@wuhaidong.me>
2022-05-10 15:53:07 +0800

    check style

--- demo/test-eslint.js
+++ demo/test-eslint.js
@@ -3,14 +3,16 @@
  * @Author       : wuhaidong
  * @Date         : 2022-05-10 10:08:02
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2022-05-10 14:53:07
+ * @LastEditTime : 2022-05-10 15:51:37
  */
 
 const add = (a, b) => {
   console.log(a + b)
   return a + b
 }
+
 const aa = 'aaaaaa'
+
 const ass = (a) => {}
 
 export default add
