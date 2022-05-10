454d2f5534b6cfb5f3cdc35e167824cedb771b71
hayden <i@wuhaidong.me>
2022-05-10 16:53:13 +0800

    check style

--- demo/test-husky.js
+++ demo/test-husky.js
@@ -3,6 +3,9 @@
  * @Author       : wuhaidong
  * @Date         : 2022-05-10 16:38:22
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2022-05-10 16:39:15
+ * @LastEditTime : 2022-05-10 16:52:37
  */
-const kongFunction = (a = {})
+const kongFunction = (a) => a + 1
+
+const num = 3
+kongFunction(num)
