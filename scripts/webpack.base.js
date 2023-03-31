ccdef20c4775ae1fbb140db76379cb611d4cc0d0
hayden <1058486292@qq.com>
2023-03-31 14:57:28 +0800

    chore: webpack 处理css、less、sass - 引入loader

--- scripts/webpack.base.js
+++ scripts/webpack.base.js
@@ -3,7 +3,7 @@
  * @Author       : wuhaidong
  * @Date         : 2023-03-28 18:00:56
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-03-30 17:40:50
+ * @LastEditTime : 2023-03-31 14:55:38
  */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
@@ -24,4 +24,62 @@ module.exports = {
       cache: false, // 特别重要：防止之后使用v6版本 copy-webpack-plugin 时代码修改一刷新页面为空问题。
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev, // 开启后与devtool设置一致，开发环境开启，生产环境关闭
+              importLoaders: 0, // 指定在css loader 处理前使用的loader数量
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev,
+              importLoaders: 1, // 需要先被less-loader 处理，所以这里设置为1
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev,
+              importLoaders: 1, // 需要先被less-loader 处理，所以这里设置为1
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@
  * @Author       : wuhaidong
  * @Date         : 2023-03-28 18:07:24
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-03-31 12:28:04
+ * @LastEditTime : 2023-03-31 14:53:37
  */
+import './app.css'
+import './app.less'
+import './app.scss'
+
 const root = document.querySelector('#root')
+
 root.innerHTML = 'hello, webpack!'
