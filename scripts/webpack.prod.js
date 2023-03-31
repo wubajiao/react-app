9e0218177a35bba95dc31b2c7f4a3bb9ee90a3be
hayden <1058486292@qq.com>
2023-03-31 14:28:07 +0800

    chore: 打包前清掉dist目录

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -3,12 +3,14 @@
  * @Author       : wuhaidong
  * @Date         : 2023-03-30 14:38:43
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-03-31 12:15:54
+ * @LastEditTime : 2023-03-31 14:25:33
  */
 const { merge } = require('webpack-merge')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const base = require('./webpack.base')
 
 module.exports = merge(base, {
   mode: 'production',
   devtool: 'nosources-source-map',
+  plugins: [new CleanWebpackPlugin()],
 })
